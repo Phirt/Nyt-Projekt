b3a65987d973495576b7223b7e051b019affcd59
Casper Lindinger <cli@asol.local>
2017-08-14 10:50:18 +0200

    Tilføjet Lav ny Template

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ $(document).ready(function () {
 
     //ADD ACTIVE CLASS TO BUTTON WHEN PRESSED
     $('#accordion .btn').on('click', function() {
-        
+        //CHECK IF CLASS HAS ACTIVE
+        if($(this).hasClass('active')) {
+            $('#accordion .btn').removeClass('active');
+            return true;
+        }
         //REMOVE ACTIVE CLASS ON ALL BUTTONS 
         $('#accordion .btn').removeClass('active');
 

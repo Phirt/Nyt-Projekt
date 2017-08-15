590dfd1570ba759d3f4455a951c502c8741b7c07
Philip Thygesen <moldo123@gmail.com>
2017-08-15 10:07:47 +0200

    Tilføk/slet labels
    Tilføjelse og sletning af labels

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
 
 
     //ADD ACTIVE CLASS TO BUTTON WHEN PRESSED
-    $('#accordion .btn').on('click', function() {
+    $('#accordion .btn').on('click', function () {
         //CHECK IF CLASS HAS ACTIVE
-        if($(this).hasClass('active')) {
+        if ($(this).hasClass('active')) {
             $('#accordion .btn').removeClass('active');
             return true;
         }
@@ -14,6 +14,37 @@ $(document).ready(function () {
         //ADD ACTIVE CLASS TO THE SPECIFIC BUTTON PRESSED
         $(this).addClass('active');
     })
+
+    //TO REMOVE A ROW IN A TABLE
+    $('.table tr button').click(function () {
+        $(this).parent().parent().remove();
+        return false;
+    });
+
+    //TO ADD A NEW LABEL
+    $('input').on('keypress', function (e) {
+        if (e.keyCode == 13) {
+            $("#myTable")
+                .append($('<tr>')
+                    .append($('<td>')
+                        .css('text-align', 'left')
+                        .append($('<button type="button" class="btn btn-default btn-circle">')
+                            .append($('<i class="glyphicon glyphicon-remove">'))
+                        )
+                        .append(' ' + $(this).val())
+                    )
+
+                )
+            $(this).val('');
+        }
+
+        //TO REMOVE A ROW IN A TABLE
+        $('.table tr button').click(function () {
+            $(this).parent().parent().remove();
+            return false;
+        });
+
+    });
 });
 
 //Hide task, person and project
@@ -35,3 +66,4 @@ $(document).ready(function () {
         }
     });
 });
+
